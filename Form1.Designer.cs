e877a7fa4b334d6065adb74d226ed52e891129c1
Bishr Arnaout <br.arnaout123@gmail.com>
2024-07-29 22:30:36 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.chkCapital_Letters = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkSmall_Letters = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkNumbers = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.chkSpecial_Letters = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.trbNumber_Of_Letters = new Guna.UI2.WinForms.Guna2VTrackBar();
+            this.lblNumber_Of_Letters = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCopy = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ContextMenuStrip1 = new Guna.UI2.WinForms.Guna2ContextMenuStrip();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guna2ContextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.Yellow;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(338, 23);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(294, 33);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Password Generator";
+            // 
+            // chkCapital_Letters
+            // 
+            this.chkCapital_Letters.AutoSize = true;
+            this.chkCapital_Letters.BackColor = System.Drawing.Color.Transparent;
+            this.chkCapital_Letters.CheckedState.BorderColor = System.Drawing.Color.Yellow;
+            this.chkCapital_Letters.CheckedState.BorderRadius = 0;
+            this.chkCapital_Letters.CheckedState.BorderThickness = 0;
+            this.chkCapital_Letters.CheckedState.FillColor = System.Drawing.Color.Yellow;
+            this.chkCapital_Letters.CheckMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.chkCapital_Letters.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.chkCapital_Letters.ForeColor = System.Drawing.Color.Yellow;
+            this.chkCapital_Letters.Location = new System.Drawing.Point(40, 183);
+            this.chkCapital_Letters.Name = "chkCapital_Letters";
+            this.chkCapital_Letters.Size = new System.Drawing.Size(156, 24);
+            this.chkCapital_Letters.TabIndex = 1;
+            this.chkCapital_Letters.Tag = "";
+            this.chkCapital_Letters.Text = "Capital Letters";
+            this.chkCapital_Letters.UncheckedState.BorderColor = System.Drawing.Color.DarkGray;
+            this.chkCapital_Letters.UncheckedState.BorderRadius = 0;
+            this.chkCapital_Letters.UncheckedState.BorderThickness = 0;
+            this.chkCapital_Letters.UncheckedState.FillColor = System.Drawing.Color.DarkGray;
+            this.chkCapital_Letters.UseVisualStyleBackColor = false;
+            // 
+            // chkSmall_Letters
+            // 
+            this.chkSmall_Letters.AutoSize = true;
+            this.chkSmall_Letters.BackColor = System.Drawing.Color.Transparent;
+            this.chkSmall_Letters.CheckedState.BorderColor = System.Drawing.Color.Yellow;
+            this.chkSmall_Letters.CheckedState.BorderRadius = 0;
+            this.chkSmall_Letters.CheckedState.BorderThickness = 0;
+            this.chkSmall_Letters.CheckedState.FillColor = System.Drawing.Color.Yellow;
+            this.chkSmall_Letters.CheckMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.chkSmall_Letters.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.chkSmall_Letters.ForeColor = System.Drawing.Color.Yellow;
+            this.chkSmall_Letters.Location = new System.Drawing.Point(40, 238);
+            this.chkSmall_Letters.Name = "chkSmall_Letters";
+            this.chkSmall_Letters.Size = new System.Drawing.Size(144, 24);
+            this.chkSmall_Letters.TabIndex = 2;
+            this.chkSmall_Letters.Tag = "";
+            this.chkSmall_Letters.Text = "Small Letters";
+            this.chkSmall_Letters.UncheckedState.BorderColor = System.Drawing.Color.DarkGray;
+            this.chkSmall_Letters.UncheckedState.BorderRadius = 0;
+            this.chkSmall_Letters.UncheckedState.BorderThickness = 0;
+            this.chkSmall_Letters.UncheckedState.FillColor = System.Drawing.Color.DarkGray;
+            this.chkSmall_Letters.UseVisualStyleBackColor = false;
+            // 
+            // chkNumbers
+            // 
+            this.chkNumbers.AutoSize = true;
+            this.chkNumbers.BackColor = System.Drawing.Color.Transparent;
+            this.chkNumbers.CheckedState.BorderColor = System.Drawing.Color.Yellow;
+            this.chkNumbers.CheckedState.BorderRadius = 0;
+            this.chkNumbers.CheckedState.BorderThickness = 0;
+            this.chkNumbers.CheckedState.FillColor = System.Drawing.Color.Yellow;
+            this.chkNumbers.CheckMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.chkNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.chkNumbers.ForeColor = System.Drawing.Color.Yellow;
+            this.chkNumbers.Location = new System.Drawing.Point(40, 293);
+            this.chkNumbers.Name = "chkNumbers";
+            this.chkNumbers.Size = new System.Drawing.Size(106, 24);
+            this.chkNumbers.TabIndex = 3;
+            this.chkNumbers.Tag = "";
+            this.chkNumbers.Text = "Numbers";
+            this.chkNumbers.UncheckedState.BorderColor = System.Drawing.Color.DarkGray;
+            this.chkNumbers.UncheckedState.BorderRadius = 0;
+            this.chkNumbers.UncheckedState.BorderThickness = 0;
+            this.chkNumbers.UncheckedState.FillColor = System.Drawing.Color.DarkGray;
+            this.chkNumbers.UseVisualStyleBackColor = false;
+            // 
+            // chkSpecial_Letters
+            // 
+            this.chkSpecial_Letters.AutoSize = true;
+            this.chkSpecial_Letters.BackColor = System.Drawing.Color.Transparent;
+            this.chkSpecial_Letters.CheckedState.BorderColor = System.Drawing.Color.Yellow;
+            this.chkSpecial_Letters.CheckedState.BorderRadius = 0;
+            this.chkSpecial_Letters.CheckedState.BorderThickness = 0;
+            this.chkSpecial_Letters.CheckedState.FillColor = System.Drawing.Color.Yellow;
+            this.chkSpecial_Letters.CheckMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.chkSpecial_Letters.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.chkSpecial_Letters.ForeColor = System.Drawing.Color.Yellow;
+            this.chkSpecial_Letters.Location = new System.Drawing.Point(40, 348);
+            this.chkSpecial_Letters.Name = "chkSpecial_Letters";
+            this.chkSpecial_Letters.Size = new System.Drawing.Size(159, 24);
+            this.chkSpecial_Letters.TabIndex = 4;
+            this.chkSpecial_Letters.Tag = "";
+            this.chkSpecial_Letters.Text = "Special Letters";
+            this.chkSpecial_Letters.UncheckedState.BorderColor = System.Drawing.Color.DarkGray;
+            this.chkSpecial_Letters.UncheckedState.BorderRadius = 0;
+            this.chkSpecial_Letters.UncheckedState.BorderThickness = 0;
+            this.chkSpecial_Letters.UncheckedState.FillColor = System.Drawing.Color.DarkGray;
+            this.chkSpecial_Letters.UseVisualStyleBackColor = false;
+            // 
+            // trbNumber_Of_Letters
+            // 
+            this.trbNumber_Of_Letters.BackColor = System.Drawing.Color.Transparent;
+            this.trbNumber_Of_Letters.FillColor = System.Drawing.Color.DarkGray;
+            this.trbNumber_Of_Letters.Location = new System.Drawing.Point(264, 130);
+            this.trbNumber_Of_Letters.Maximum = 40;
+            this.trbNumber_Of_Letters.Minimum = 4;
+            this.trbNumber_Of_Letters.Name = "trbNumber_Of_Letters";
+            this.trbNumber_Of_Letters.Size = new System.Drawing.Size(30, 300);
+            this.trbNumber_Of_Letters.TabIndex = 5;
+            this.trbNumber_Of_Letters.ThumbColor = System.Drawing.Color.Yellow;
+            this.trbNumber_Of_Letters.Value = 4;
+            this.trbNumber_Of_Letters.Scroll += new System.Windows.Forms.ScrollEventHandler(this.trbNumber_Of_Letters_Scroll);
+            // 
+            // lblNumber_Of_Letters
+            // 
+            this.lblNumber_Of_Letters.AutoSize = false;
+            this.lblNumber_Of_Letters.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumber_Of_Letters.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber_Of_Letters.ForeColor = System.Drawing.Color.Yellow;
+            this.lblNumber_Of_Letters.Location = new System.Drawing.Point(338, 174);
+            this.lblNumber_Of_Letters.Name = "lblNumber_Of_Letters";
+            this.lblNumber_Of_Letters.Size = new System.Drawing.Size(164, 33);
+            this.lblNumber_Of_Letters.TabIndex = 6;
+            this.lblNumber_Of_Letters.Text = "4 Letters";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Yellow;
+            this.txtPassword.ContextMenuStrip = this.guna2ContextMenuStrip1;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txtPassword.Location = new System.Drawing.Point(338, 307);
+            this.txtPassword.MaxLength = 40;
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(605, 30);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnGenerate.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnGenerate.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnGenerate.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnGenerate.FillColor = System.Drawing.Color.Yellow;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold);
+            this.btnGenerate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGenerate.Location = new System.Drawing.Point(499, 379);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(180, 45);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCopy.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCopy.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCopy.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCopy.FillColor = System.Drawing.Color.Yellow;
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold);
+            this.btnCopy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCopy.Location = new System.Drawing.Point(763, 379);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(180, 45);
+            this.btnCopy.TabIndex = 9;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // guna2ContextMenuStrip1
+            // 
+            this.guna2ContextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.guna2ContextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.guna2ContextMenuStrip1.Name = "guna2ContextMenuStrip1";
+            this.guna2ContextMenuStrip1.RenderStyle.ArrowColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.BorderColor = System.Drawing.Color.Gainsboro;
+            this.guna2ContextMenuStrip1.RenderStyle.ColorTable = null;
+            this.guna2ContextMenuStrip1.RenderStyle.RoundedEdges = true;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionArrowColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SeparatorColor = System.Drawing.Color.Gainsboro;
+            this.guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.guna2ContextMenuStrip1.Size = new System.Drawing.Size(123, 52);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Password_Generator.Properties.Resources.photo_2024_07_29_13_51_10;
+            this.ClientSize = new System.Drawing.Size(981, 475);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblNumber_Of_Letters);
+            this.Controls.Add(this.trbNumber_Of_Letters);
+            this.Controls.Add(this.chkSpecial_Letters);
+            this.Controls.Add(this.chkNumbers);
+            this.Controls.Add(this.chkSmall_Letters);
+            this.Controls.Add(this.chkCapital_Letters);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.guna2ContextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2CheckBox chkCapital_Letters;
+        private Guna.UI2.WinForms.Guna2CheckBox chkSmall_Letters;
+        private Guna.UI2.WinForms.Guna2CheckBox chkNumbers;
+        private Guna.UI2.WinForms.Guna2CheckBox chkSpecial_Letters;
+        private Guna.UI2.WinForms.Guna2VTrackBar trbNumber_Of_Letters;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblNumber_Of_Letters;
+        private System.Windows.Forms.TextBox txtPassword;
+        private Guna.UI2.WinForms.Guna2Button btnGenerate;
+        private Guna.UI2.WinForms.Guna2Button btnCopy;
+        private Guna.UI2.WinForms.Guna2ContextMenuStrip guna2ContextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private bool[] CheckedTypes = new bool[4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void trbNumber_Of_Letters_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblNumber_Of_Letters.Text = trbNumber_Of_Letters.Value.ToString() + " Letters";
+        }
+        bool IsAnyTypeChecked()
+        {
+            if (chkCapital_Letters.Checked || chkSmall_Letters.Checked
+                || chkNumbers.Checked || chkSpecial_Letters.Checked)
+                return true;
+            return false;
+        }
+        char GetRandomCharacter(byte From, byte To)
+        {
+            return Convert.ToChar(random.Next(From, To));
+        }
+        byte GetRandomType()
+        {
+            return Convert.ToByte(random.Next(0, 4));
+        }
+        void GeneratePassword()
+        {
+            for (byte i = 1; i <= trbNumber_Of_Letters.Value; i++)
+            {
+                byte type = GetRandomType();
+
+                 while (CheckedTypes[type] != true)
+                {
+                    type = GetRandomType();
+                }
+
+                 switch(type)
+                {
+                    case 0:
+                        {
+                            txtPassword.Text += GetRandomCharacter(65, 90);
+                            break;
+                        }
+                    case 1:
+                        {
+                            txtPassword.Text += GetRandomCharacter(97, 122);
+                            break;
+                        }
+                    case 2:
+                        {
+                            txtPassword.Text += GetRandomCharacter(48, 57);
+                            break;
+                        }
+                    case 3:
+                        {
+                            byte SpecialGroub = GetRandomType();
+                            switch (SpecialGroub)
+                            {
+                                case 0:
+                                    txtPassword.Text += GetRandomCharacter(33, 47);
+                                    break;
+                                case 1:
+                                    txtPassword.Text += GetRandomCharacter(58, 64);
+                                    break;
+                                case 2:
+                                    txtPassword.Text += GetRandomCharacter(91, 96);
+                                    break;
+                                case 3:
+                                    txtPassword.Text += GetRandomCharacter(123, 126);
+                                    break;
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+        void CheckeCheckedTypes()
+        {
+            if (chkCapital_Letters.Checked) CheckedTypes[0] = true;
+            else CheckedTypes[0] = false;
+            if (chkSmall_Letters.Checked) CheckedTypes[1] = true;
+            else CheckedTypes[1] = false;
+            if (chkNumbers.Checked) CheckedTypes[2] = true;
+            else CheckedTypes[2] = false;
+            if (chkSpecial_Letters.Checked) CheckedTypes[3] = true;
+            else CheckedTypes[3] = false;
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+            if (IsAnyTypeChecked())
+            {
+                CheckeCheckedTypes();
+                GeneratePassword();
+            }
+            else
+                MessageBox.Show("Choose any type!", "No selected type", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Text)) Clipboard.SetText(txtPassword.Text);
+            else
+                MessageBox.Show("There is no password to be copied!", "No password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Text)) Clipboard.SetText(txtPassword.Text);
+            else
+                MessageBox.Show("There is no password to be copied!", "No password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+        }
+    }
+}
